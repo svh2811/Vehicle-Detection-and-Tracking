f4b0468130c23cabccd900d2ee3c5ba193aa0de7
Sarvesh Upadhyay <sarvesh.a.upadhyay@gmail.com>
2018-10-07 13:04:37 -0400

    Insert: First Commit

---
+++ data/download_ds.sh
@@ -0,0 +1,11 @@
+# M
+wget https://s3.amazonaws.com/udacity-sdc/Vehicle_Tracking/vehicles.zip
+unzip vehicles.zip
+rm vehicles.zip
+
+#54.93 M
+wget https://s3.amazonaws.com/udacity-sdc/Vehicle_Tracking/non-vehicles.zip
+unzip non-vehicles.zip
+rm non-vehicles.zip
+
+rm -r _MACOSX
